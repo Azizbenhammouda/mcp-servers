c06c2767ad96eab9d715231987b4fb9b7211c43b
Aziz Ben Hammouda <azizbhammouda@gmail.com>
2026-07-30 05:03:38 +0100

    get_repo tool

--- MCP-SERVER-1/Repository/repository_tools.go
+++ MCP-SERVER-1/Repository/repository_tools.go
@@ -2,18 +2,62 @@ package Repository
 
 import (
 	"context"
+	"fmt"
+	"io"
+	"net/http"
 
 	"github.com/modelcontextprotocol/go-sdk/mcp"
 )
 
-type DummyInput struct {
-	Name string
+type GetRepositoryInput struct {
+	Owner string `json:"owner"`
+	Name  string `json:"name"`
 }
 
-func Ping(ctx context.Context, req *mcp.CallToolRequest, input DummyInput) (*mcp.CallToolResult, any, error) {
+func GetRepository(
+	ctx context.Context,
+	req *mcp.CallToolRequest,
+	input GetRepositoryInput,
+) (*mcp.CallToolResult, any, error) {
+
+	url := fmt.Sprintf(
+		"https://api.github.com/repos/%s/%s",
+		input.Owner,
+		input.Name,
+	)
+
+	httpReq, err := http.NewRequestWithContext(
+		ctx,
+		http.MethodGet,
+		url,
+		nil,
+	)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to create request: %v", err)
+	}
+
+	client := &http.Client{}
+
+	resp, err := client.Do(httpReq)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to create request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, nil, fmt.Errorf("failed to create request: %v", err)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to create request: %v", err)
+	}
+
 	return &mcp.CallToolResult{
 		Content: []mcp.Content{
-			&mcp.TextContent{Text: "pong"},
+			&mcp.TextContent{
+				Text: string(body),
+			},
 		},
 	}, nil, nil
 }
